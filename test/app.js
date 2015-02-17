6cb56b25498791d4e15a03cb4ceb0a32eb6a81fe
mattdesl <dave.des@gmail.com>
2015-02-17 10:22:41 -0500

    testing live reload

--- test/app.js
+++ test/app.js
@@ -8,8 +8,8 @@ var time = 0
 function render(ctx, width, height, dt) {
     time += dt/1000
     ctx.clearRect(0, 0, width, height)
-    ctx.fillRect(Math.sin(time)*50 + 100, 15, 250, 250) 
-    ctx.fillText("foo taada!", 20, 40)
+    ctx.fillRect(Math.sin(time)*50 + 200, 35, 150, 150) 
+    ctx.fillText("foo taada!", 100, 40)
     if (img.width > 0 || img.height > 0)
         ctx.drawImage(img, 50, 50)
 }
