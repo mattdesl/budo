510f85a537d529b1747be6df9a72ecdffe44f480
mattdesl <dave.des@gmail.com>
2015-02-17 01:15:42 -0500

    updating deps and cleaning up code

--- test/app.js
+++ test/app.js
@@ -9,7 +9,7 @@ function render(ctx, width, height, dt) {
     time += dt/1000
     ctx.clearRect(0, 0, width, height)
     ctx.fillRect(Math.sin(time)*50 + 100, 15, 250, 250) 
-    ctx.fillText("foo taada!", 40, 40)
+    ctx.fillText("foo taada!", 20, 40)
     if (img.width > 0 || img.height > 0)
-        ctx.drawImage(img, 250, 50)
+        ctx.drawImage(img, 50, 50)
 }
