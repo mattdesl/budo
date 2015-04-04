1644fb5d6ba8daf51fe8567f3350ed9e43a0e173
mattdesl <dave.des@gmail.com>
2015-04-03 20:16:21 -0400

    splitting out some modules

--- example/app.js
+++ example/app.js
@@ -5,8 +5,8 @@ var fit = require('canvas-fit')(ctx.canvas, window, dpr)
 //setup canvas DOM state
 window.addEventListener('resize', fit, false)
 require('domready')(function() {
-    fit()
-    document.body.appendChild(ctx.canvas)
+  fit()
+  document.body.appendChild(ctx.canvas)
 })
 
 var img = new Image()
@@ -14,17 +14,17 @@ img.src = 'baboon.png'
 var time = 0
 
 require('raf-loop')(function(dt) {
-    var width = ctx.canvas.width,
-        height = ctx.canvas.height
-    ctx.clearRect(0, 0, width, height)
+  var width = ctx.canvas.width,
+    height = ctx.canvas.height
+  ctx.clearRect(0, 0, width, height)
 
-    time += dt/1000
+  time += dt/1000
 
-    ctx.save()
-    ctx.scale(dpr, dpr)
-    ctx.fillRect(Math.sin(time)*50 + 300, 200, 500, 105) 
-    ctx.fillText("from browserify!", 40, 40)
-    if (img.width > 0 || img.height > 0)
-        ctx.drawImage(img, 50, 50)
-    ctx.restore()
+  ctx.save()
+  ctx.scale(dpr, dpr)
+  ctx.fillRect(Math.sin(time)*50 + 300, 50, 100, 100) 
+  ctx.fillText("from browserify!", 40, 40)
+  if (img.width > 0 || img.height > 0)
+    ctx.drawImage(img, 50, 50)
+  ctx.restore()
 }).start()
