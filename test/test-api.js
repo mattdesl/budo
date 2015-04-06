6fdc8770bb6259f4140b21c308f17cc3c27e1789
mattdesl <dave.des@gmail.com>
2015-04-05 21:34:43 -0400

    fixing tests, reworking some stuff

--- test/test-api.js
+++ test/test-api.js
@@ -3,58 +3,59 @@ var budo = require('../')
 var cleanup = require('./cleanup')
 var path = require('path')
 
-// test('gets update() after connect', function(t) {
-//   t.plan(8)
-//   t.timeoutAfter(10000)
-
-//   budo('test/app.js', {
-//     dir: __dirname,
-//     port: 8000
-//   })
-//   .on('error', function(err) {
-//     t.fail(err)
-//   })
-//   .on('connect', function(app) {
-//     var file = path.join(__dirname, 'bundle.js')
-//     t.equal(app.to, 'bundle.js', 'mapping matches')
-//     t.equal(app.from, file, 'from matches')
-//     t.equal(app.uri, 'http://localhost:8000/', 'uri matches')
-//     t.equal(app.host, 'localhost', 'host is not specified')
-//     t.equal(app.port, 8000, 'port matches')
-//     t.equal(app.dir, __dirname, 'dir matches')
-
-//     app
-//       .once('update', function(type) {
-//         t.ok(true, 'got bundle.js update')
-//         app.close()
-//         cleanup()
-//       })
-//   })
-//   .on('reload', function() {
-//     t.fail('should not have received reload event')
-//   })
-//   .on('exit', function() {
-//     t.ok(true, 'closing')
-//   })
-// })
-
-test('sets live() with args before connect', function(t) {
-  t.plan(4)
+test('gets connect info', function(t) {
+  t.plan(7)
   t.timeoutAfter(10000)
 
   var app = budo('test/app.js', {
     dir: __dirname,
-    port: 8000,
-    outfile: 'bundle.js'
+    port: 8000
   })
-    //manually enable LiveReload server
-    .live() 
-    //manually trigger LiveReload event on bundle.js update
-    .on('update', function() {
-      t.ok('got live reload event')
-      app.reload()
-    })
-  testLive(t, app)
+  .on('error', function(err) {
+    t.fail(err)
+  })
+  .on('connect', function(ev) {
+    t.deepEqual(ev.entries, [ 'test/app.js' ], 'entries matches')
+    t.equal(ev.serve, 'bundle.js', 'mapping matches')
+    t.equal(ev.uri, 'http://localhost:8000/', 'uri matches')
+    t.equal(ev.host, 'localhost', 'host is not specified')
+    t.equal(ev.port, 8000, 'port matches')
+    t.equal(ev.dir, __dirname, 'dir matches')
+    app.close()
+  })
+  .on('reload', function() {
+    t.fail('should not have received reload event')
+  })
+  .on('watch', function() {
+    t.fail('should not have received watch event')
+  })
+  .on('exit', function() {
+    t.ok(true, 'closing')
+  })
+})
+
+test('entry mapping', function(t) {
+  t.plan(2)
+  t.timeoutAfter(10000)
+  
+  var app = budo(['test/app:foo.js', 'test/other.js'])
+  .on('connect', function(ev) {
+    t.equal(ev.serve, 'foo.js', 'mapping matches')
+    t.deepEqual(ev.entries, ['test/app', 'test/other.js'], 'from matches')
+    app.close()
+  })
+})
+
+test('--serve allows explicit bundle renaming', function(t) {
+  t.plan(2)
+  t.timeoutAfter(10000)
+  
+  var app = budo(['test/app', 'test/other.js'], { serve: 'static/foo.js' })
+  .on('connect', function(ev) {
+    t.equal(ev.serve, 'static/foo.js', 'mapping matches')
+    t.deepEqual(ev.entries, ['test/app', 'test/other.js'], 'from matches')
+    app.close()
+  })
 })
 
 test('sets watch() and live() by default with live: true', function(t) {
@@ -66,18 +67,36 @@ test('sets watch() and live() by default with live: true', function(t) {
     port: 8000,
     live: true
   })
+    .once('update', function() { 
+      //bundle.js changed
+      t.ok(true, 'got update event')
+    })
+  testLive(t, app)
+})
+
+test('allow setting live() manually', function(t) {
+  t.plan(3)
+  t.timeoutAfter(10000)
+
+  var app = budo('test/app.js', {
+    dir: __dirname,
+    port: 8000,
+    live: true
+  })
+    .live() //start live server
+    .on('update', function() {
+      t.ok(true, 'got first update')
+      app.reload()
+    })
   testLive(t, app)
 })
 
 function testLive(t, app) {
   app
-    .once('update', function() { //bundle.js changed
-      t.ok(true, 'got update event')
-    })
-    .once('reload', function(err) { //LiveReload triggered
+    .once('reload', function(err) { 
+      //LiveReload triggered
       t.ok(true, 'got reload event')
       app.close()
-      cleanup()
     })
     .on('error', function(err) {
       t.fail(err)
