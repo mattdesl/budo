b37c84172ae032b49c27fb80868eb03058bd289c
mattdesl <dave.des@gmail.com>
2015-04-11 12:58:59 -0400

    no longer using ecstatic or caching for bundles

--- example/app.js
+++ example/app.js
@@ -22,7 +22,7 @@ require('raf-loop')(function(dt) {
 
   ctx.save()
   ctx.scale(dpr, dpr)
-  ctx.fillRect(Math.sin(time)*50 + 300, 50, 20, 20) 
+  ctx.fillRect(Math.sin(time)*50 + 300, 50, 20, 40) 
   ctx.fillText("from browserify!", 40, 40)
   if (img.width > 0 || img.height > 0)
     ctx.drawImage(img, 50, 50)
