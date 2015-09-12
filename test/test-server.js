c3c6f2d505ea9b9d6bb708db4266dcbeb6d56463
mattdesl <dave.des@gmail.com>
2015-09-12 13:39:51 -0400

    add test for defaultIndex

--- test/test-server.js
+++ test/test-server.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var budo = require('../')
+var defaultHtml = require('simple-html-index')
 
 var request = require('request')
 var entry = 'test/fixtures/app.js'
@@ -16,6 +17,34 @@ test('should serve on --dir', function (t) {
       }, function (err, resp, body) {
         if (err) t.fail(err)
         t.equal(body.toString(), 'foobar', 'text matches')
+        app.close()
+      })
+    })
+    .on('exit', function () {
+      t.ok(true, 'closed')
+    })
+    .on('error', t.fail.bind(t))
+})
+
+test('support defaultIndex stream', function (t) {
+  t.plan(3)
+
+  function html (opt) {
+    return defaultHtml({
+      entry: opt.entry,
+      title: 'foobar',
+      css: 'main.css'
+    })
+  }
+
+  var app = budo(entry, { dir: __dirname, defaultIndex: html })
+    .on('connect', function (ev) {
+      request.get({
+        uri: ev.uri
+      }, function (err, resp, body) {
+        if (err) t.fail(err)
+        t.equal(resp.statusCode, 200)
+        t.equal(body, '<!doctype html><head><title>foobar</title><meta charset="utf-8"><link rel="stylesheet" href="main.css"></head><body><script src="app.js"></script></body></html>')
         app.close()
       })
     })
