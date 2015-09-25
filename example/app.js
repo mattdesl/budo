b821c16dbde48a7c3760e5549afdeedfb70860f8
mattdesl <dave.des@gmail.com>
2015-09-25 10:02:59 -0400

    clean up example, use es6

--- example/app.js
+++ example/app.js
@@ -1,32 +1,31 @@
 /*globals Image*/
-var dpr = window.devicePixelRatio || 1
-var ctx = require('2d-context')()
-var fit = require('canvas-fit')(ctx.canvas, window, dpr)
+import createLoop from 'canvas-loop'
+import createContext from '2d-context'
 
-// setup canvas DOM state
-window.addEventListener('resize', fit, false)
-require('domready')(function () {
-  fit()
-  document.body.appendChild(ctx.canvas)
+const context = createContext()
+const canvas = context.canvas
+
+const app = createLoop(canvas, {
+  scale: window.devicePixelRatio
 })
+document.body.appendChild(canvas)
 
-var img = new Image()
+const img = new Image()
+img.onload = () => app.start()
 img.src = 'baboon.png'
-var time = 0
 
-require('raf-loop')(function (dt) {
-  var width = ctx.canvas.width
-  var height = ctx.canvas.height
-  ctx.clearRect(0, 0, width, height)
+let time = 0
+
+app.on('tick', (dt) => {
+  const [ width, height ] = app.shape
+  context.clearRect(0, 0, width, height)
 
   time += dt / 1000
 
-  ctx.save()
-  ctx.scale(dpr, dpr)
-  ctx.fillRect(Math.sin(time) * 50 + 300, 50, 20, 40)
-  ctx.fillText('from browserify!', 40, 40)
-  if (img.width > 0 || img.height > 0) {
-    ctx.drawImage(img, 50, 50)
-  }
-  ctx.restore()
-}).start()
+  context.save()
+  context.scale(app.scale, app.scale)
+  context.fillRect(Math.sin(time) * 50 + 300, 50, 20, 40)
+  context.fillText('from browserify!', 40, 40)
+  context.drawImage(img, 50, 50)
+  context.restore()
+})
