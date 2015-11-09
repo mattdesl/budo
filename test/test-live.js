8572c22886bc932b06cc75e232c728372fa05ebb
mattdesl <dave.des@gmail.com>
2015-11-09 12:30:25 -0500

    ensure that defaults match in CLI and API; also update tap-spec

--- test/test-live.js
+++ test/test-live.js
@@ -108,9 +108,9 @@ function matchesHTML (t, uri, html, cb) {
 }
 
 function getHTMLNoLive () {
-  return '<!doctype html><head><meta charset="utf-8"></head><body><script src="app.js"></script></body></html>'
+  return '<!doctype html><head><title>budo</title><meta charset="utf-8"></head><body><script src="app.js"></script></body></html>'
 }
 
 function getHTML () {
-  return '<!doctype html><head><meta charset="utf-8"></head><body><script type="text/javascript" src="http://localhost:35729/livereload.js?snipver=1"></script><script src="app.js"></script></body></html>'
+  return '<!doctype html><head><title>budo</title><meta charset="utf-8"></head><body><script type="text/javascript" src="http://localhost:35729/livereload.js?snipver=1"></script><script src="app.js"></script></body></html>'
 }
