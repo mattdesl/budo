d7719042dae60fec7614e4ad1f341a834c79f639
mattdesl <dave.des@gmail.com>
2015-11-10 11:37:44 -0500

    fix entry mapping for windows and letter drive absolute paths

---
+++ test/test-map-entry.js
@@ -0,0 +1,14 @@
+var test = require('tape')
+var mapEntry = require('../lib/map-entry')
+
+test('should map entry paths', function (t) {
+  t.deepEqual(mapEntry('foo.js'), { from: 'foo.js', url: 'foo.js' })
+  t.deepEqual(mapEntry('foo/bar.js'), { from: 'foo/bar.js', url: 'bar.js' })
+  t.deepEqual(mapEntry('foo/bar.js:bundle.js'), { from: 'foo/bar.js', url: 'bundle.js' })
+  t.deepEqual(mapEntry('foo/bar.js:bundle.js?foo'), { from: 'foo/bar.js', url: 'bundle.js?foo' })
+  t.deepEqual(mapEntry('/absolute/path.js:bundle.js?foo'), { from: '/absolute/path.js', url: 'bundle.js?foo' })
+  t.deepEqual(mapEntry('/absolute/path.js'), { from: '/absolute/path.js', url: 'path.js' })
+  t.deepEqual(mapEntry('C:/absolute/path.js'), { from: 'C:/absolute/path.js', url: 'path.js' })
+  t.deepEqual(mapEntry('C:/absolute/path.js:bundle.js'), { from: 'C:/absolute/path.js', url: 'bundle.js' })
+  t.end()
+})
