92f0cf8e36edcb2e3ccf20d23bd2e8fdb32fed3d
Anand Thakker <vestibule@anandthakker.net>
2015-12-17 12:24:18 -0500

    Move bole.reset() from startup to shutdown

--- test/test-log.js
+++ test/test-log.js
@@ -6,12 +6,14 @@ var entry = 'test/fixtures/app.js'
 
 test('should log properly after restarting the server', function (t) {
   var out = through.obj(function (item, _, next) {
-    t.ok(item, 'log message')
+    if (item.name === 'budo' && item.type === 'connect') {
+      t.ok(item, 'budo start log message')
+    }
     next()
   })
 
-  // 2 messages for each server run: 1 for server startup and 1 for bundle complete
-  t.plan(4)
+  // should only get two server startup messages
+  t.plan(2)
   // start a self-closing server, and then do it again (once) when it exits
   start().once('exit', start)
 
