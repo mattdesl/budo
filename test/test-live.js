ddc52b267b9e13bed47e0c270e49a7fc2bd13ec6
mattdesl <dave.des@gmail.com>
2016-01-18 15:09:52 -0500

    small tweaks and new live test for address

--- test/test-live.js
+++ test/test-live.js
@@ -4,6 +4,7 @@ var path = require('path')
 var request = require('request')
 var fs = require('fs')
 var source = fs.readFileSync(path.join(__dirname, 'fixtures', 'app.js'), 'utf8')
+var internalIp = require('internal-ip')
 
 test('should inject LiveReload snippet', function (t) {
   t.plan(4)
@@ -98,6 +99,32 @@ test('should not inject LiveReload snippet', function (t) {
     })
 })
 
+test('LiveReload snippet host should default to budo host', function (t) {
+  t.plan(2)
+  t.timeoutAfter(10000)
+
+  var addr = internalIp()
+  var entry = path.join(__dirname, 'fixtures', 'app.js')
+  var app = budo(entry, {
+    dir: __dirname,
+    port: 8000,
+    host: addr,
+    serve: 'app.js'
+  })
+    .live()
+    .on('error', function (err) {
+      t.fail(err)
+    })
+    .on('connect', function (ev) {
+      matchesHTML(t, ev.uri, getHTMLWithHost(addr), function () {
+        app.close()
+      })
+    })
+    .on('exit', function () {
+      t.ok(true, 'closing')
+    })
+})
+
 function matchesHTML (t, uri, html, cb) {
   request.get({ uri: uri + 'index.html' }, function (err, resp, body) {
     if (err) t.fail(err)
@@ -113,4 +140,8 @@ function getHTMLNoLive () {
 
 function getHTML () {
   return '<!doctype html><head><title>budo</title><meta charset="utf-8"></head><body><script type="text/javascript" src="//localhost:35729/livereload.js?snipver=1" async="" defer=""></script><script src="app.js"></script></body></html>'
+}
+
+function getHTMLWithHost (ip) {
+  return '<!doctype html><head><title>budo</title><meta charset="utf-8"></head><body><script type="text/javascript" src="//' + ip + ':35729/livereload.js?snipver=1" async="" defer=""></script><script src="app.js"></script></body></html>'
 }
