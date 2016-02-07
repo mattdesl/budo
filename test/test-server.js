9199226cd9821c7b030fc0fcff97a1e852a534f1
Callum <callum@callumj.co.uk>
2016-02-07 16:24:24 +0000

    prepend base tag to head when pushstate is enabled

--- test/test-server.js
+++ test/test-server.js
@@ -78,6 +78,34 @@ test('support --title and --css', function (t) {
     .on('error', t.fail.bind(t))
 })
 
+test('support --pushstate', function (t) {
+  t.plan(3)
+
+  function html (opt) {
+    return defaultHtml({
+      entry: opt.entry,
+      title: 'foobar',
+      css: 'main.css'
+    })
+  }
+
+  var app = budo(entry, { dir: __dirname, defaultIndex: html, pushstate: true })
+    .on('connect', function (ev) {
+      request.get({
+        uri: ev.uri
+      }, function (err, resp, body) {
+        if (err) t.fail(err)
+        t.equal(resp.statusCode, 200)
+        t.equal(body, '<!doctype html><head><base href="/"><title>foobar</title><meta charset="utf-8"><link rel="stylesheet" href="main.css"></head><body><script src="app.js"></script></body></html>')
+        app.close()
+      })
+    })
+    .on('exit', function () {
+      t.ok(true, 'closed')
+    })
+    .on('error', t.fail.bind(t))
+})
+
 test('favicon.ico should have status code 200', function (t) {
   t.plan(2)
   var app = budo(entry, { dir: __dirname })
