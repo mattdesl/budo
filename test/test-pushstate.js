a7dd8b3c3c955b899e503b05a5d3720cd506622b
mattdesl <dave.des@gmail.com>
2016-04-26 09:41:32 -0400

    add test for push state; fix serve option shorthand

---
+++ test/test-pushstate.js
@@ -0,0 +1,22 @@
+var test = require('tape')
+var budo = require('../')
+var path = require('path')
+var request = require('request')
+
+var file = path.join(__dirname, 'fixtures', 'app.js')
+var html = '<!doctype html><head><title>budo</title><meta charset="utf-8"></head><body><script src="app.js"></script></body></html>'
+
+test('pushstate', function (t) {
+  t.plan(1)
+  var b = budo(file, {
+    port: 9966,
+    pushstate: true,
+    portfind: false
+  }).on('connect', function (ev) {
+    request({ uri: ev.uri + '/foobar' }, function (err, resp, body) {
+      b.close()
+      if (err) return t.fail(err)
+      t.equal(body, html, 'returns home index.html')
+    })
+  })
+})
