57300f4b5e05cfa01ff822d4b2cf3b044b775e70
Christian Talmo <talmo.christian@gmail.com>
2016-06-03 00:47:14 +0300

    fixed double path bug in proxy, improved tests, longer timeout

--- test/test-proxy.js
+++ test/test-proxy.js
@@ -4,12 +4,13 @@ var path = require('path')
 var request = require('request')
 var http = require('http')
 
-test('connect connect to proxy server api', function (t) {
-  t.plan(4)
-  t.timeoutAfter(6000)
+test('connect to proxy server api', function (t) {
+  t.plan(6)
+  t.timeoutAfter(20000)
 
   var server = http.createServer(function (req, res) {
-    // console.log('url: %s', req.url);
+    // console.log('url: %s', req.url)
+    t.equal(req.url, '/api/hello')
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.write(JSON.stringify({ message: 'hello world' }))
     res.end()
@@ -19,15 +20,14 @@ test('connect connect to proxy server api', function (t) {
     t.ok(true, 'closing proxy server')
   })
 
-  server.listen(9977, function () {
+  server.listen(3000, function () {
     var entry = path.join(__dirname, 'fixtures', 'app.js')
     var app = budo(entry, {
       dir: __dirname,
       port: 8000,
       host: 'localhost',
       serve: 'app.js',
-      live: true,
-      proxy: '/api@http://localhost:9977/api'
+      proxy: '/api@http://localhost:3000/api'
     })
       .on('error', function (err) {
         t.fail(err)
@@ -35,7 +35,9 @@ test('connect connect to proxy server api', function (t) {
       .on('connect', function (ev) {
         setTimeout(function () {
           request('http://localhost:8000/api/hello', function (error, response, body) {
+            if (error) t.fail(error)
             t.ok(!error, 'no proxy error')
+            t.ok(body, 'received proxy response body')
             var json = JSON.parse(body)
             t.equal(json.message, 'hello world')
             app.close()
