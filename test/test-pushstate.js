1c84ccaa3f5cd418402742ce598ee52d347ff8f7
Maximilian Antoni <mail@maxantoni.de>
2017-03-20 20:54:56 +0100

    Add pushstate-allow and pushstate-disallow

--- test/test-pushstate.js
+++ test/test-pushstate.js
@@ -6,7 +6,7 @@ var request = require('request')
 var file = path.join(__dirname, 'fixtures', 'app.js')
 var html = '<!DOCTYPE html><html lang="en" dir="ltr"><head><title>budo</title><meta charset="utf-8"></head><body><script src="app.js"></script></body></html>'
 
-test('pushstate', function (t) {
+test('pushstate flag', function (t) {
   t.plan(1)
   var b = budo(file, {
     port: 9966,
@@ -16,6 +16,53 @@ test('pushstate', function (t) {
     request({ uri: ev.uri + '/foobar' }, function (err, resp, body) {
       b.close()
       if (err) return t.fail(err)
+      t.equal(body, html, 'returns home index.html')
+    })
+  })
+})
+
+test('pushstate serve static', function (t) {
+  t.plan(1)
+  var b = budo(file, {
+    port: 9966,
+    pushstate: true,
+    portfind: false
+  }).on('connect', function (ev) {
+    request({ uri: ev.uri + 'app.js' }, function (err, resp, body) {
+      b.close()
+      if (err) return t.fail(err)
+      t.equal(resp.statusCode, 200, '200 OK')
+    })
+  })
+})
+
+test('pushstate-allow', function (t) {
+  t.plan(1)
+  var b = budo(file, {
+    port: 9966,
+    pushstate: true,
+    pushstateAllow: '/app.js',
+    portfind: false
+  }).on('connect', function (ev) {
+    request({ uri: ev.uri + '/app.js' }, function (err, resp, body) {
+      b.close()
+      if (err) return t.fail(err)
+      t.equal(resp.statusCode, 404, '404 Not found')
+    })
+  })
+})
+
+test('pushstate-disallow', function (t) {
+  t.plan(2)
+  var b = budo(file, {
+    port: 9966,
+    pushstateDisallow: '/foo.bar',
+    portfind: false
+  }).on('connect', function (ev) {
+    request({ uri: ev.uri + '/foo.bar' }, function (err, resp, body) {
+      b.close()
+      if (err) return t.fail(err)
+      t.equal(resp.statusCode, 200, '200 OK')
       t.equal(body, html, 'returns home index.html')
     })
   })
